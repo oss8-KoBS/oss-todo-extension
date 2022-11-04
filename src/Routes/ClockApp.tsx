5232e7ce867373f75315adfefcfc22a6bb8329b5
Yeongmin_Lee <peter2326@naver.com>
2022-11-04 09:34:37 +0900

    Update ClockApp.tsx
    clock

--- src/Routes/ClockApp.tsx
+++ src/Routes/ClockApp.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
 `;
+const Clock = styled.h1`
+  text-align: center;
+  position: relative;
+  top: 50px;
+  font-size: 100px;
+`;
 
 function ClockApp() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect (() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
     <Wrapper>
-      <h1>Clock App Page!</h1>
+      <Clock>
+        {time.toLocaleTimeString('en-US')}
+      </Clock>
     </Wrapper>
   );
 }
